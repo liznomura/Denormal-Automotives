64242deda7923e3289f53b1db22ac65c19ef8772
liznomura <lizn@hawaii.edu>
2017-07-25 09:19:16 -1000

    step 6

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,13 +1,17 @@
-DROP USER IF EXISTS denormal_user;
-DROP DATABASE IF EXISTS denormal_cars;
+-- DROP USER IF EXISTS denormal_user;
+-- DROP DATABASE IF EXISTS denormal_cars;
 
-CREATE USER denormal_user;
+-- CREATE USER denormal_user;
 
-CREATE DATABASE denormal_cars WITH OWNER denormal_user;
+-- CREATE DATABASE denormal_cars WITH OWNER denormal_user;
 
-\c denormal_cars;
+-- \c denormal_cars;
 
-\i scripts/denormal_data.sql;
+-- \i scripts/denormal_data.sql;
 
-SELECT DISTINCT make_title
-FROM car_models;
+-- SELECT DISTINCT make_title
+-- FROM car_models;
+
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code = 'VOLKS';
