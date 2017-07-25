f1b7758e70074f62a9e0e7a7bc07145d3944c0b2
liznomura <lizn@hawaii.edu>
2017-07-25 09:20:30 -1000

    step 7

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -12,6 +12,10 @@
 -- SELECT DISTINCT make_title
 -- FROM car_models;
 
-SELECT DISTINCT model_title
+-- SELECT DISTINCT model_title
+-- FROM car_models
+-- WHERE make_code = 'VOLKS';
+
+SELECT DISTINCT make_code, model_code, model_title, year
 FROM car_models
-WHERE make_code = 'VOLKS';
+WHERE make_code = 'LAM';
