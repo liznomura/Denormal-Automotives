0f9af224a1575770decc12cb62598b2783809a8e
liznomura <lizn@hawaii.edu>
2017-07-25 14:16:19 -1000

    inserted into car_year

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,47 +1,52 @@
--- DROP USER IF EXISTS normal_user;
--- DROP DATABASE IF EXISTS normal_cars;
+DROP USER IF EXISTS normal_user;
+DROP DATABASE IF EXISTS normal_cars;
 
--- CREATE USER normal_user;
--- CREATE DATABASE normal_cars WITH OWNER normal_user;
+CREATE USER normal_user;
+CREATE DATABASE normal_cars WITH OWNER normal_user;
 
--- \c normal_cars;
+\c normal_cars;
 
--- DROP TABLE IF EXISTS car_makes CASCADE;
--- CREATE TABLE car_makes (
---   make_id SERIAL NOT NULL PRIMARY KEY,
---   make_code VARCHAR(50) NOT NULL,
---   make_title VARCHAR(100) NOT NULL
--- );
+DROP TABLE IF EXISTS car_makes CASCADE;
+CREATE TABLE car_makes (
+  make_id SERIAL NOT NULL PRIMARY KEY,
+  make_code VARCHAR(50) NOT NULL,
+  make_title VARCHAR(100) NOT NULL
+);
 
--- DROP TABLE IF EXISTS car_models CASCADE;
--- CREATE TABLE car_models (
---   model_id SERIAL NOT NULL PRIMARY KEY,
---   model_code VARCHAR(50) NOT NULL,
---   model_title VARCHAR(100) NOT NULL,
---   car_make_code INT REFERENCES car_make(make_id) NOT NULL
--- );
+DROP TABLE IF EXISTS car_models CASCADE;
+CREATE TABLE car_models (
+  model_id SERIAL NOT NULL PRIMARY KEY,
+  model_code VARCHAR(50) NOT NULL,
+  model_title VARCHAR(100) NOT NULL,
+  car_make_code INT REFERENCES car_makes(make_id) NOT NULL
+);
 
--- DROP TABLE IF EXISTS car_year CASCADE;
--- CREATE TABLE car_year (
---   year_id INT NOT NULL PRIMARY KEY,
---   year INT NOT NULL
--- );
+DROP TABLE IF EXISTS car_year CASCADE;
+CREATE TABLE car_year (
+  year_id INT NOT NULL PRIMARY KEY,
+  year INT NOT NULL
+);
 
--- DROP TABLE IF EXISTS model_years CASCADE;
--- CREATE TABLE model_years (
---   car_id SERIAL NOT NULL PRIMARY KEY,
---   car_model_key INT REFERENCES car_model(model_id) NOT NULL,
---   car_year_key INT REFERENCES car_year(year_id) NOT NULL
--- );
+DROP TABLE IF EXISTS model_years CASCADE;
+CREATE TABLE model_years (
+  car_id SERIAL NOT NULL PRIMARY KEY,
+  car_model_key INT REFERENCES car_models(model_id) NOT NULL,
+  car_year_key INT REFERENCES car_year(year_id) NOT NULL
+);
 
--- \i scripts/denormal_data.sql;
+\i scripts/denormal_data.sql;
 
--- INSERT INTO car_makes (make_code, make_title)
--- SELECT DISTINCT make_code, make_title
--- FROM denormal_data
--- ORDER BY make_code;
+INSERT INTO car_makes (make_code, make_title)
+SELECT DISTINCT d.make_code, d.make_title
+FROM denormal_data d
+ORDER BY make_code;
 
 INSERT INTO car_models (model_code, model_title, car_make_code)
 SELECT DISTINCT d.model_code, d.model_title, (SELECT m.make_id FROM car_makes m WHERE m.make_code = d.make_code)
 FROM denormal_data d
-ORDER BY car_make_code;
+ORDER BY model_code;
+
+INSERT INTO car_year (year_id, year)
+SELECT DISTINCT (year % 100), d.year
+FROM denormal_data d
+ORDER BY year;
